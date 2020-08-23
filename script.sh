c1f1317a66ea2e98c69bb17b92b9db2136047788
MitchellWeg <mitchell.w@live.nl>
2020-08-23 19:23:23 +0200

    fix script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 # this will download a 39mb lichess database PGN file.
-curl https://database.lichess.org/standard/lichess_db_standard_rated_2013-07.pgn.bz2
+curl https://database.lichess.org/standard/lichess_db_standard_rated_2013-07.pgn.bz2 --output lichess-database.pgn.bz2
 
 # decompress it with bz2, and keep the old file
-bz2 -dk lichess_db_standard_rated_2013-07.pgn.bz2
+bzip2 -dk lichess-database.pgn.bz2
 
-python main.py lichess_db_standard_rated_2013-07.pgn
+# call the main parser script.
+python main.py lichess-database.pgn lichessdb.json
 
  
